82ae82a0e9c4a4f4b3d4c653490218df9b499c56
matma6 (inĝ. Mikaelo Gabor) <matma6@matma6.net>
2016-08-11 21:11:35 +0200

    Oni povas legi metadatumojn

--- kodo/legu.pl
+++ kodo/legu.pl
@@ -1,7 +1,30 @@
 :- use_module(library(http/json)).
 
+:- dynamic lega_hoko/2.
+
 legu_json(Vojo, Dikto) :-
 	setup_call_cleanup(
 		open(Vojo, read, Fluo, [encoding(utf8)]),
 		json_read_dict(Fluo, Dikto),
 		close(Fluo)).
+
+metu_tradukojn(Vojo, Dikto) :-
+	assert(traduko(Vojo, Lingvo, Atributo, Dikto.Lingvo.Atributo)), fail.
+
+metu_atributon(Vojo, tradukoj, Dikto) :- !,
+	metu_tradukojn(Vojo, Dikto), fail.
+
+metu_atributon(Vojo, Atributo, Valoro) :-
+	assert(atributo(Vojo, Atributo, Valoro)), fail.
+
+metu_atributojn(Vojo, Dikto) :-
+	metu_atributon(Vojo, Atributo, Dikto.Atributo); true.
+
+metu_tekstojn(_).
+
+legu_paghon(Vojo) :-
+	string_concat(Vojo, "/meta.json", Meta),
+	legu_json(Meta, Dikto),
+	metu_atributojn(Vojo, Dikto),
+	metu_tekstojn(Vojo),
+	(lega_hoko(Vojo, Dikto); true).
