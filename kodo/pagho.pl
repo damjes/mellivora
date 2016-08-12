12623a8b237fdd5c8235ca204ef185018a484c0f
matma6 (inĝ. Mikaelo Gabor) <matma6@matma6.net>
2016-08-12 14:58:37 +0200

    Aldoni implicitan malplenan tekston por paĝoj

--- kodo/pagho.pl
+++ kodo/pagho.pl
@@ -33,9 +33,15 @@ valoro(Id, Lingvo, Nomo, Valoro) :-
 	traduko(Id, Lingvo, Nomo, Valoro).
 
 valoro(Id, Lingvo, Nomo, Valoro) :-
-	fonta_teksto(Id, Lingvo, Ero, Vojo),
+	lingvo(Id, Lingvo),
 	dosiernomo(Ero, Nomo),
-	=(Vojo, Valoro). % FARENDA (TODO)
+	(
+			fonta_teksto(Id, Lingvo, Ero, Vojo)
+		*->
+			=(Vojo, Valoro) % FARENDA (TODO)
+		;
+			Valoro = ""
+	).
 
 generu_dikton(Id, Lingvo, Dikto) :-
 	findall(Nomo-Valoro, valoro(Id, Lingvo, Nomo, Valoro), Paroj),
