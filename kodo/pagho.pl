934e8975b2f0f36fdb07d84ac5e400698de1a414
matma6 (inĝ. Mikaelo Gabor) <matma6@matma6.net>
2016-08-12 14:29:44 +0200

    Interŝanĝo de partoj

--- kodo/pagho.pl
+++ kodo/pagho.pl
@@ -1,5 +1,30 @@
+radiko(Id) :-
+	patro(Id, _),
+	\+ patro(_, Id), !.
+
+vojo(Id, _, X, X) :-
+	radiko(Id).
+
+vojo(Id, Lingvo, X, Y) :-
+	valoro(Id, Lingvo, slugo, Slugo),
+	patro(Patro, Id),
+	vojo(Patro, Lingvo, X, [Slugo | Y]).
+
+gluu_vojon(Fino, Patro, Vojo) :-
+	atom_string(Fino, FinCheno),
+	string_concat(Patro, "/", Patro2),
+	string_concat(Patro2, FinCheno, Vojo).
+
+vojcheno(Id, Lingvo, VojCheno) :-
+	atom_string(Lingvo, LingvCheno),
+	vojo(Id, Lingvo, Vojo, []),
+	foldl(gluu_vojon, Vojo, LingvCheno, VojCheno).
+
 dosiernomo("1", unua).
 dosiernomo("2", dua).
+
+valoro(Id, Lingvo, vojo, Vojo) :-
+	vojo(Id, Lingvo, Vojo, []).
 
 valoro(Id, _, Nomo, Valoro) :-
 	atributo(Id, Nomo, Valoro).
@@ -15,15 +40,3 @@ valoro(Id, Lingvo, Nomo, Valoro) :-
 generu_dikton(Id, Lingvo, Dikto) :-
 	findall(Nomo-Valoro, valoro(Id, Lingvo, Nomo, Valoro), Paroj),
 	dict_pairs(Dikto, pagho, Paroj).
-
-radiko(Id) :-
-	patro(Id, _),
-	\+ patro(_, Id), !.
-
-vojo(Id, _, X, X) :-
-	radiko(Id).
-
-vojo(Id, Lingvo, X, Y) :-
-	valoro(Id, Lingvo, slugo, Slugo),
-	patro(Patro, Id),
-	vojo(Patro, Lingvo, X, [Slugo | Y]).
