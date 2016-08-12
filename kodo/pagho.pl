c4084491552d829427460b4153cf25283a6f8709
matma6 (inĝ. Mikaelo Gabor) <matma6@matma6.net>
2016-08-12 12:53:20 +0200

    Aldonita valoro/2 por generi diktojn

---
+++ kodo/pagho.pl
@@ -0,0 +1,13 @@
+dosiernomo("1", unua).
+dosiernomo("2", dua).
+
+valoro(Id, _, Nomo, Valoro) :-
+	atributo(Id, Nomo, Valoro).
+
+valoro(Id, Lingvo, Nomo, Valoro) :-
+	traduko(Id, Lingvo, Nomo, Valoro).
+
+valoro(Id, Lingvo, Nomo, Valoro) :-
+	fonta_teksto(Id, Lingvo, Ero, Vojo),
+	dosiernomo(Ero, Nomo),
+	=(Vojo, Valoro). % FARENDA (TODO)
