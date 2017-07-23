6a36f9d401e9c15874da3d9f8a3499c5d9172a44
Damjes <git@damj.es>
2017-07-23 22:08:59 +0200

    Blibonigi mellivora.pl

--- mellivora.pl
+++ mellivora.pl
@@ -3,21 +3,25 @@
 % UZU SWI-PROLOG >= 7
 % swipl -s mellivora [-t celo]
 
-:- ['kodo/konsili.pl'].
+konsilu :-
+	['kodo/konsili.pl'].
 
 rulu :-
+	konsilu,
 	legu_dosieron(fonto, _ID),
 	generu_retejon,
 	halt.
 
+instalu_aldonajhojn :-
+	pack_install(simple_template, [interactive(false), update(true)]).
+
 kompilu :-
-	write('Kompilas programon...'),
+	instalu_aldonajhojn,
+	konsilu,
+	writeln('Kompilas programon...'),
 	set_prolog_flag(optimise, true),
 	qsave_program(mellivora, [
 		foreign(save),
 		goal(rulu),
 		stand_alone(true)]),
-	writeln(' OK!').
-
-instalu_aldonajhojn :-
-	pack_install(simple_template, [update(true)]).
+	writeln('Programo pretas!').
